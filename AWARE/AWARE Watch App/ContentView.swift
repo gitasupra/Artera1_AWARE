c3ea7560de53a513459e1e176c1a674f2cb2c635
jessica-lieu <jessicalieu@ucsb.edu>
2023-11-05 17:00:38 -0800

    Initialized Swift project
    Initialized for both WatchOS with Companion iOS app

---
+++ AWARE/AWARE Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AWARE Watch App
+//
+//  Created by Jessica Lieu on 11/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
