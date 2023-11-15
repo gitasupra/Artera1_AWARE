6f15eebb8d58ae0d85ea7ccf9006e5c879272bad
Cheryl Stanley <cherylstanley28@gmail.com>
2023-11-14 18:06:56 -0800

    initial ui

--- AWARE/AWARE Watch App/ContentView.swift
+++ AWARE/AWARE Watch App/ContentView.swift
@@ -1,24 +1,85 @@
-//
-//  ContentView.swift
-//  AWARE Watch App
-//
-//  Created by Jessica Lieu on 11/5/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var enableDataCollection = false
+    @State private var shouldHide = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            TabView {
+                // Page 1
+                Page1View()
+                    .tabItem {
+                        Label("AWARE", systemImage: "person.circle.fill")
+                    }
+
+                // Page 2
+                Page2View(enableDataCollection: $enableDataCollection, shouldHide: $shouldHide)
+                    .tabItem {
+                        Label("Page 2", systemImage: "info.circle")
+                    }
+            }
+            .navigationTitle("AWARE App")
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct Page1View: View {
+
+
+    var body: some View {
+        VStack {
+            Text("AWARE")
+                .font(.largeTitle)
+                .padding()
+            Image(systemName: "person.circle.fill")
+                .font(.system(size: 100)) // Adjust the font size to make the image bigger
+                .foregroundColor(.gray)
+                .padding()
+
+
+            
+        }
+    }
 }
+
+struct Page2View: View {
+    @Binding var enableDataCollection: Bool
+    @Binding var shouldHide: Bool
+    var body: some View {
+        VStack {
+//            Text("Page 2 Content")
+//                .font(.largeTitle)
+//                .padding()
+            
+            if (enableDataCollection) {
+                if !shouldHide {
+                    Text("Disable Data Collection on your Apple Watch")
+                    Button {
+                        enableDataCollection.toggle()
+                        print(enableDataCollection)
+                    } label: {
+                        Image(systemName: "touchid")
+                            .font(.system(size: 50))
+                            .foregroundColor(.white)
+                            .background(Color.red)
+                            .controlSize(.extraLarge)
+                    }
+                }
+            } else {
+                Text("Enable Data Collection on your Apple Watch")
+                Button {
+                    enableDataCollection.toggle()
+                    print(enableDataCollection)
+                } label: {
+                    Image(systemName: "touchid")
+                        .font(.system(size: 50))
+                        .foregroundColor(.white)
+                        .background(Color.green)
+                        .controlSize(.extraLarge)
+                }
+            }
+        }
+    }
+}
+
--- AWARE/AWARE/ContentView.swift
+++ AWARE/AWARE/ContentView.swift
@@ -1,24 +1,115 @@
-//
-//  ContentView.swift
-//  AWARE
-//
-//  Created by Jessica Lieu on 11/5/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var enableDataCollection = false
+    @State private var shouldHide = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            // Page 1 - Home / Toggle
+            VStack(alignment: .center) {
+                Text("AWARE")
+                    .font(.system(size: 36)) // Adjust the font size for the title
+                Image(systemName: "person.circle.fill")
+                    .font(.system(size: 100)) // Adjust the font size to make the image bigger
+                    .foregroundColor(.gray)
+                    .padding()
+                
+                if enableDataCollection {
+                    if !self.$shouldHide.wrappedValue {
+                        Text("Disable Data Collection")
+                            .padding()
+                        Button {
+                            enableDataCollection.toggle()
+                            print(enableDataCollection)
+                        } label: {
+                            Image(systemName: "touchid")
+                                .font(.system(size: 50)) // Adjust the font size for the button image
+                                .foregroundColor(.white)
+                                .background(Color.red)
+                                .controlSize(.extraLarge)
+                        }
+                    }
+                } else {
+                    Text("Enable Data Collection")
+                        .padding()
+                    Button {
+                        enableDataCollection.toggle()
+                        print(enableDataCollection)
+                    } label: {
+                        Image(systemName: "touchid")
+                            .font(.system(size: 50)) // Adjust the font size for the button image
+                            .foregroundColor(.white)
+                            .background(Color.green)
+                            .controlSize(.extraLarge)
+                    }
+                }
+            }
+            .tabItem {
+                Label("Home", systemImage: "1.circle")
+            }
+            
+            // Page 2 Graphs
+            NavigationView {
+                VStack(alignment: .center) {
+                    Text("Graphs")
+                        .font(.system(size: 36))
+                    
+                    NavigationLink(destination: Text("Heart Rate Data")) {
+                        Button("View Heart Rate Data") {}
+                    }
+                    
+                    NavigationLink(destination: Text("Breathing Rate Data")) {
+                        Button("View Breathing Rate Data") {}
+                    }
+                    
+                    NavigationLink(destination: Text("Walking Steadiness Data")) {
+                        Button("View Walking Steadiness Data") {}
+                    }
+                }
+            }
+            .tabItem {
+                Label("Graphs", systemImage: "2.circle")
+            }
+            
+            // Page 3 User
+            VStack(alignment: .center) {
+                Text("User")
+                    .font(.system(size: 36))
+            }
+            .tabItem {
+                Label("User", systemImage: "3.circle")
+            }
+            
+            // Page 4 Today
+            NavigationView {
+                VStack(alignment: .center) {
+                    Text("Today")
+                        .font(.system(size: 36))
+                    
+                    NavigationLink(destination: Text("Past Holistic Drunkenness Data Collection")) {
+                        Button("View Past Holistic Drunkenness Data Collection") {}
+                    }
+                }
+            }
+            .tabItem {
+                Label("Today", systemImage: "4.circle")
+            }
+            
+            // Page 5 Settings
+            VStack(alignment: .center) {
+                Text("Settings")
+                    .font(.system(size: 36))
+            }
+            .tabItem {
+                Label("Settings", systemImage: "5.circle")
+            }
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 }
