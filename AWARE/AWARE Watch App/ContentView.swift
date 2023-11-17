900d0ac7d07d44acbcb932a468dcfdf11247aaf5
Ritvi Bhatt <ritvi.bhatt@gmail.com>
2023-11-16 20:38:25 -0800

    rb - add startAccelerometer and startGyroscope data collection capability

--- AWARE/AWARE Watch App/ContentView.swift
+++ AWARE/AWARE Watch App/ContentView.swift
@@ -1,4 +1,6 @@
 import SwiftUI
+import HealthKit
+import CoreMotion
 
 struct ContentView: View {
     @State private var enableDataCollection = false
@@ -22,6 +24,7 @@ struct ContentView: View {
             .navigationTitle("AWARE App")
         }
     }
+    
 }
 
 struct Page1View: View {
@@ -46,6 +49,8 @@ struct Page1View: View {
 struct Page2View: View {
     @Binding var enableDataCollection: Bool
     @Binding var shouldHide: Bool
+    @EnvironmentObject var motion: CMMotionManager
+
     var body: some View {
         VStack {
 //            Text("Page 2 Content")
@@ -80,7 +85,73 @@ struct Page2View: View {
                 }
             }
         }
+        .onChange(of: enableDataCollection)
+        {
+            if (enableDataCollection) {
+                startAccelerometer()
+                startGyroscope()
+            } else {
+                self.motion.stopAccelerometerUpdates()
+                self.motion.stopGyroUpdates()
+            }
+        }
     }
+    
+    func startAccelerometer() {
+            
+            
+            if motion.isAccelerometerAvailable {
+                self.motion.accelerometerUpdateInterval = 1.0 / 50.0
+                self.motion.startAccelerometerUpdates()
+                
+                // Configure a timer to fetch the accelerometer data
+                let timer = Timer(fire: Date(), interval: (1.0 / 50.0), repeats: true,
+                                   block: { (timer) in
+                    if let data = self.motion.accelerometerData {
+                        // Get the acceleration data
+                        let accelerationX = data.acceleration.x
+                        let accelerationY = data.acceleration.y
+                        let accelerationZ = data.acceleration.z
+                        
+                        print("Acceleration x: ", accelerationX)
+                        print("Acceleration y: ", accelerationY)
+                        print("Acceleration z: ", accelerationZ)
+                    }
+                })
+                
+                // Add the timer to the current run loop
+                RunLoop.current.add(timer, forMode: RunLoop.Mode.default)
+            }
+            
+        }
+    
+    func startGyroscope() {
+            
+            
+            if motion.isGyroAvailable {
+                self.motion.gyroUpdateInterval = 1.0 / 50.0
+                self.motion.startGyroUpdates()
+                
+                // Configure a timer to fetch the gyroscope data
+                let timer = Timer(fire: Date(), interval: (1.0 / 50.0), repeats: true,
+                                   block: { (timer) in
+                    if let data = self.motion.gyroData {
+                        // Get the gyroscope data
+                        let gyroX = data.rotationRate.x
+                        let gyroY = data.rotationRate.y
+                        let gyroZ = data.rotationRate.z
+                        
+                        print("Rotation x: ", gyroX)
+                        print("Rotation y: ", gyroY)
+                        print("Rotation z: ", gyroZ)
+                    }
+                })
+                
+                // Add the timer to the current run loop
+                RunLoop.current.add(timer, forMode: RunLoop.Mode.default)
+            }
+            
+        }
 }
 
 #Preview{
