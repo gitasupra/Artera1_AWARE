c4d378a627083ca4e5700c4083741c82701493e4
Gita Supramaniam <gsupramaniam@umail.ucsb.edu>
2024-01-17 15:50:50 -0800

    remove DS_Store, add Swift Package Manager + swiftpm for Alamofire

---
+++ Tests/Artera1_AWARETests/Artera1_AWARETests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Artera1_AWARE
+
+final class Artera1_AWARETests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
