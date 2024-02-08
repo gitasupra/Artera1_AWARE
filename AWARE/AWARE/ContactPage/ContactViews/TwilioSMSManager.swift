cb3e196948ad3a524640f87240c1bdf098a705b8
Jessica Nguyen <jessicanguyen@ucsb.edu>
2024-02-08 14:50:58 -0800

    format text for different levels, added link to google maps location

--- AWARE/AWARE/ContactPage/ContactViews/TwilioSMSManager.swift
+++ AWARE/AWARE/ContactPage/ContactViews/TwilioSMSManager.swift
@@ -15,19 +15,25 @@ class TwilioSMSManager {
     private var TwilioToken: String = ""
     private var username: String = ""
     
+    private var locationManager: LocationManager
+    private var latitude: Double
+    private var longitude: Double
+    
     init() {
-        // Get a reference to your Firebase Realtime Database
+        self.locationManager = LocationManager.shared
+        self.latitude = self.locationManager.userLocation?.coordinate.latitude ?? 38.898150
+        self.longitude = self.locationManager.userLocation?.coordinate.longitude ?? -77.034340
+        
         let twilioRef = Database.database().reference().child("twilio_credentials")
         
-        // Read the data from the "twilio_credentials" node
+        // read the data from the "twilio_credentials" in Firebase
         twilioRef.observeSingleEvent(of: .value, with: { snapshot in
-            // Check if the snapshot contains any data
             guard let value = snapshot.value as? [String: Any] else {
                 print("No data available")
                 return
             }
             
-            // Extract account SID and auth token from the snapshot
+            // extract account SID and auth token from the snapshot
             if let accountSID = value["account_sid"] as? String,
                let authToken = value["auth_token"] as? String {
                 self.TwilioSID = accountSID
@@ -45,31 +51,33 @@ class TwilioSMSManager {
     }
     
     func sendSMS(level: Int, contactsManager: ContactsManager) {
+        // set different messages based on intoxication level
         var message: String
         if level == 0 {
             message = "AWARE Alert: \(self.username) has added you as an emergency contact. Please stay alert for future updates."
         } else if level == 1 {
-            message = "Attention: Our biometric analysis indicates that \(self.username) is tipsy. Please keep an eye out for any changes in their condition."
+            message = "Attention: Our analysis indicates that \(self.username) is tipsy. Please keep an eye out for any changes in their condition. Open Google Maps to view their current location: https://www.google.com/maps/dir/?api=1&destination=\(self.latitude),\(self.longitude)&travelmode=driving"
         } else if level == 2 {
-            message = "Urgent: \(self.username) is estimated to have high levels of intoxication. Immediate assistance may be required. Please check in or offer support as needed."
+            message = "URGENT: \(self.username) is estimated to have high levels of intoxication. Immediate assistance may be required. Please check in or offer support as needed. Open Google Maps to view their current location: https://www.google.com/maps/dir/?api=1&destination=\(self.latitude),\(self.longitude)&travelmode=driving"
         } else if level == 3 {
-            message = "Emergency: \(self.username)'s vital signs indicate a critical condition. Emergency services have been alerted. Please respond immediately."
+            message = "EMERGENCY: \(self.username)'s vital signs indicate a critical condition. Emergency services have been alerted. Please respond immediately. Open Google Maps to view their current location: https://www.google.com/maps/dir/?api=1&destination=\(self.latitude),\(self.longitude)&travelmode=driving"
         } else {
             return
         }
         
+        // pull list of phone numbers from user's contact list
         let phoneNumbers = contactsManager.contacts.compactMap { contact -> String? in
             guard let firstCharacter = contact.phone.first else { return nil }
             let processedPhoneNumber = contact.phone.dropFirst().replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
             return String(firstCharacter) + processedPhoneNumber
         }
         
+        // send SMS to all contacts using Twilio's API
         if !self.TwilioSID.isEmpty && !self.TwilioToken.isEmpty {
             let url = "https://api.twilio.com/2010-04-01/Accounts/\(self.TwilioSID)/Messages"
             
             for phoneNumber in phoneNumbers {
                 let parameters = ["From": "+18667644137", "To": "\(phoneNumber)", "Body": message]
-                
                 AF.request(url, method: .post, parameters: parameters)
                     .authenticate(username: self.TwilioSID, password: self.TwilioToken)
                     .responseData { response in
@@ -85,7 +93,6 @@ class TwilioSMSManager {
                         }
                     }
             }
-            
             RunLoop.main.run()
         } else {
             debugPrint("ERROR: Unable to get TWILIO_ACCOUNT_SID or TWILIO_AUTH_TOKEN from environment variables")
